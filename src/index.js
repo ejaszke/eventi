c06ee3de46c87042c51e283eb53a7fb52ef6a379
Edward Jaszke <edward.jaszke@gmail.com>
2016-10-12 16:16:11 +0200

    set heroku port -fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const app = require('./app');
-const port = app.get('port');
+const port = process.env.PORT || app.get('port');
 
-const server = process.env.PORT || app.listen(port);
+const server = app.listen(port);
 
 server.on('listening', () =>
   console.log(`Feathers application started on ${app.get('host')}:${port}`)
